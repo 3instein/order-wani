9826e324234f72edbfe3e0bcb880620c056860a7
Reynaldi Kindarto <37333204+script404@users.noreply.github.com>
2023-09-28 17:02:38 +0700

    Food Menu Data Fetching

--- app/(menu)/page.tsx
+++ app/(menu)/page.tsx
@@ -1,45 +1,56 @@
-import { FC } from "react";
-import { MenuItem } from "@/components/Menu/MenuItem";
+import { FC, useEffect } from "react"
+import { MenuItem } from "@/components/Menu/MenuItem"
+import { Menu } from "@/interfaces/Menu"
+import axios from "axios"
 
-const Foods: FC = () => {
+const data: Menu[] = [
+    {
+        name: "Ayam Geprek",
+        price: 10000
+    }
+]
+
+async function getMenus() {
+    const response = await axios.get('http://localhost:3000/menus')
+
+    if (response.data.status === 200) {
+        return response.data.data
+    }
+}
+
+const Foods: FC = async () => {
+
+    const menus: Menu[] = await getMenus()
+
     return (
         <>
-            <div className="grid grid-cols-1 md:grid-cols-12 gap-4 gap-y-8">
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-                <div className="md:col-span-6 lg:col-span-4">
-                    <MenuItem />
-                </div>
-            </div>
-            <div className="join md:float-right mt-5 flex justify-center items-center">
-                <button className="join-item btn">1</button>
-                <button className="join-item btn btn-active">2</button>
-                <button className="join-item btn">3</button>
-                <button className="join-item btn">4</button>
-            </div>
-
+            {
+                menus.length === 0 ? (
+                    <div className="flex justify-center items-center h-1/2">
+                        <p className="text-2xl font-bold">Tidak ada menu</p>
+                    </div>
+                )
+                    :
+                    (
+                        <div className="grid grid-cols-1 md:grid-cols-12 gap-4 gap-y-8">
+                            {
+                                menus.map((menu, index) => (
+                                    <MenuItem key={index} menu={menu} />
+                                ))
+                            }
+                        </div>
+                    )
+            }
+            {
+                menus.length > 0 && menus.length > 6 && (
+                    <div className="join md:float-right mt-5 flex justify-center items-center">
+                        <button className="join-item btn">1</button>
+                        <button className="join-item btn btn-active">2</button>
+                        <button className="join-item btn">3</button>
+                        <button className="join-item btn">4</button>
+                    </div>
+                )
+            }
         </>
     )
 }
--- components/Menu/MenuItem.tsx
+++ components/Menu/MenuItem.tsx
@@ -1,16 +1,21 @@
 import { FC } from "react";
 import AyamGeprek from "public/assets/ayam-geprek.jpg"
 import Image from "next/image";
+import { Menu } from "@/interfaces/Menu";
 
-const MenuItem: FC = () => {
+interface MenuItemProps {
+    menu: Menu
+}
+
+const MenuItem: FC<MenuItemProps> = ({ menu }) => {
     return (
         <div className="card md:w-48 lg:w-52 xl:w-72 2xl:w-96 mx-auto md:mx-0 bg-base-100 shadow-xl hover:shadow-2xl cursor-pointer transition-transform transform-gpu hover:scale-105">
             <figure className="relative">
                 <Image src={AyamGeprek} alt="Shoes" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">Ayam Geprek</h2>
-                <p>Rp. 15.000</p>
+                <h2 className="card-title">{menu.name}</h2>
+                <p>Rp. {menu.price}</p>
             </div>
         </div>
 
